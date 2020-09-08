511f040c261c54feebf3a7eea02dd060d7941dc0
Fanjin Zhang <zfjsail@163.com>
2020-09-08 19:37:12 +0800

    a

--- exp/sup_moe_train.py
+++ exp/sup_moe_train.py
@@ -40,9 +40,9 @@ argparser.add_argument("--lr", type=float, default=1e-4)
 argparser.add_argument("--lambda_entropy", type=float, default=0.0)
 argparser.add_argument("--lambda_moe", type=float, default=1)
 argparser.add_argument("--base_model", type=str, default="rnn")
-argparser.add_argument("--attn-type", type=str, default="onehot")
+argparser.add_argument("--attn-type", type=str, default="cor")
 argparser.add_argument('--train-num', default=None, help='Number of training samples')
-argparser.add_argument('--n-try', type=int, default=5, help='Repeat Times')
+argparser.add_argument('--n-try', type=int, default=1, help='Repeat Times')
 
 argparser.add_argument('--embedding-size', type=int, default=128,
                        help="Embeding size for LSTM layer")
