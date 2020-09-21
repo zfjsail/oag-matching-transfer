3abb8b9933cb09b8ac03bfdee32847f9d11721e7
Fanjin Zhang <zfjsail@163.com>
2020-09-21 14:36:42 +0800

    baseline venue

--- exp/baseline.py
+++ exp/baseline.py
@@ -1,5 +1,6 @@
 from os.path import join
 import pickle
+import json
 from collections import Counter
 import numpy as np
 import sklearn
@@ -32,6 +33,12 @@ def load_aff_data():
     return pairs, labels
 
 
+def load_venue_data():
+    file_dir = settings.VENUE_DATA_DIR
+    train_data = json.load(open(join(settings.VENUE_DATA_DIR, 'train_filter.txt'), 'r'))
+    return train_data
+
+
 def fit_tfidf_for_aff():
     corpus = []
     pairs, _ = load_aff_data()
@@ -43,6 +50,21 @@ def fit_tfidf_for_aff():
     X = vectorizer.fit_transform(corpus)
     out_dir = join(settings.OUT_DIR, "aff")
     with open(join(out_dir, "aff_tfidf.pkl"), "wb") as wf:
+        pickle.dump(vectorizer, wf)
+
+
+def fit_tfidf_for_venue():
+    corpus = []
+    pairs = load_venue_data()
+    print(len(pairs), pairs)
+    for p in pairs:
+        # print(p)
+        corpus.append(p[2].lower())
+        corpus.append(p[1].lower())
+    vectorizer = TfidfVectorizer()
+    X = vectorizer.fit_transform(corpus)
+    out_dir = join(settings.OUT_DIR, "venue")
+    with open(join(out_dir, "venue_tfidf.pkl"), "wb") as wf:
         pickle.dump(vectorizer, wf)
 
 
@@ -87,6 +109,74 @@ def aff_keyword_method():
     labels_valid = labels[n_train: (n_valid + n_train)]
     features_test = features[(n_valid + n_train): ]
     labels_test = labels[(n_valid + n_train):]
+
+    precs, recs, thrs = precision_recall_curve(labels_valid, features_valid)
+    f1s = 2 * precs * recs / (precs + recs)
+    f1s = f1s[:-1]
+    thrs = thrs[~np.isnan(f1s)]
+    f1s = f1s[~np.isnan(f1s)]
+    best_thr = thrs[np.argmax(f1s)]
+    print("best thr", best_thr)
+
+    y_pred = np.zeros_like(labels_test)
+    y_pred[features_test > best_thr] = 1
+
+    prec, rec, f1, _ = precision_recall_fscore_support(labels_test, y_pred, average="binary")
+    auc = roc_auc_score(labels_test, features_test)
+    print("AUC: %.4f Prec: %.4f Rec: %.4f F1: %.4f" %
+          (auc, prec, rec, f1))
+
+
+def venue_keyword_method():
+    pairs = load_venue_data()
+    out_dir = join(settings.OUT_DIR, "venue")
+    with open(join(out_dir, "venue_tfidf.pkl"), "rb") as rf:
+        vectorizer = pickle.load(rf)
+    print(vectorizer.vocabulary_)
+    vocab = vectorizer.vocabulary_
+    idf = vectorizer.idf_
+
+    # pairs = sklearn.utils.shuffle(
+    #     pairs, random_state=42
+    # )
+
+    train_num = 800
+    test_num = 200
+
+    n_pos_set = int((train_num + 2 * test_num) / 2)
+
+    neg_pairs = [p for p in pairs if p[0] == 0]
+    pos_pairs = [p for p in pairs if p[0] == 1][-n_pos_set:]
+    n_pos = len(pos_pairs)
+    neg_pairs = neg_pairs[-n_pos:]
+    train_data = pos_pairs + neg_pairs
+
+    train_data = sklearn.utils.shuffle(train_data, random_state=37)
+
+    labels = [x[0] for x in train_data]
+
+    features = []
+    for i, p in enumerate(train_data):
+        aff1 = p[2].lower()
+        aff2 = p[1].lower()
+        aff1_words = feature_utils.get_words(aff1)
+        aff2_words = feature_utils.get_words(aff2)
+        intersec = Counter(aff1_words) & Counter(aff2_words)
+        and_idf = sum([intersec[w] * idf[vocab[w]] for w in intersec if w in vocab])
+        aff1_idf = sum([idf[vocab[w]] for w in aff1_words if w in vocab])
+        aff2_idf = sum([idf[vocab[w]] for w in aff2_words if w in vocab])
+        cur_jac = and_idf/(aff1_idf+aff2_idf-and_idf)
+        features.append(cur_jac)
+
+    n = len(pairs)
+    n_train = train_num
+    n_valid = test_num
+    features_valid = features[n_train: (n_valid + n_train)]
+    labels_valid = labels[n_train: (n_valid + n_train)]
+    features_test = features[(n_valid + n_train): ]
+    labels_test = labels[(n_valid + n_train):]
+
+    print("valid", len(labels_valid),  "test", len(labels_test))
 
     precs, recs, thrs = precision_recall_curve(labels_valid, features_valid)
     f1s = 2 * precs * recs / (precs + recs)
@@ -162,6 +252,94 @@ def aff_svm():
     print(prec, rec, f1)
 
 
+def venue_svm():
+    pairs = load_venue_data()
+    out_dir = join(settings.OUT_DIR, "venue")
+    with open(join(out_dir, "venue_tfidf.pkl"), "rb") as rf:
+        vectorizer = pickle.load(rf)
+    print(vectorizer.vocabulary_)
+    vocab = vectorizer.vocabulary_
+    idf = vectorizer.idf_
+
+    train_num = 800
+    test_num = 200
+
+    n_pos_set = int((train_num + 2 * test_num) / 2)
+
+    neg_pairs = [p for p in pairs if p[0] == 0]
+    pos_pairs = [p for p in pairs if p[0] == 1][-n_pos_set:]
+    n_pos = len(pos_pairs)
+    neg_pairs = neg_pairs[-n_pos:]
+    train_data = pos_pairs + neg_pairs
+
+    train_data = sklearn.utils.shuffle(train_data, random_state=37)
+
+    labels = [x[0] for x in train_data]
+
+    features = []
+    for i, p in enumerate(train_data):
+        aff1 = p[2].lower()
+        aff2 = p[1].lower()
+        aff1_words = feature_utils.get_words(aff1)
+        aff2_words = feature_utils.get_words(aff2)
+        intersec = Counter(aff1_words) & Counter(aff2_words)
+        and_idf = sum([intersec[w] * 1 for w in intersec if w in vocab])
+        aff1_idf = sum([1 for w in aff1_words if w in vocab])
+        aff2_idf = sum([1 for w in aff2_words if w in vocab])
+        cur_jac = and_idf/(aff1_idf+aff2_idf-and_idf)
+
+        aff_idf_vec = vectorizer.fit_transform([aff1, aff2])
+        cos = cosine_similarity(aff_idf_vec)
+
+        cur_v_mag = aff1
+        cur_v_aminer = aff2
+        overlap = set(cur_v_mag).intersection(cur_v_aminer)
+        new_seq_mag = []
+        new_seq_aminer = []
+        for w in cur_v_mag:
+            if w in overlap:
+                new_seq_mag.append(w)
+        for w in cur_v_aminer:
+            if w in overlap:
+                new_seq_aminer.append(w)
+
+        intersec = Counter(new_seq_aminer) & Counter(new_seq_mag)
+        and_idf = sum([intersec[w] * 1 for w in intersec if w in vocab])
+        aminer_idf_key = sum([1 for w in new_seq_aminer if w in vocab])
+        mag_idf_key = sum([1 for w in new_seq_mag if w in vocab])
+        mother = aminer_idf_key+mag_idf_key-and_idf
+        if mother != 0:
+            cur_jac_key = and_idf/mother
+            print("key jac", cur_jac_key)
+            aff_idf_vec = vectorizer.fit_transform([" ".join(new_seq_aminer), " ".join(new_seq_mag)])
+            cos_key = cosine_similarity(aff_idf_vec)
+        else:
+            cur_jac_key = 0
+            cos_key = -1
+            # print("here")
+
+        # cur_feat = [cur_jac, cos[0, 1], cur_jac_key, cos_key]
+        cur_feat = [cur_jac, cos[0, 1]]
+        # print("cur feature", cur_feat)
+        features.append(cur_feat)
+
+    features = np.array(features)
+    # n = len(pairs)
+    n_train = train_num
+    n_valid = test_num
+    features_train = features[: n_train + n_valid]
+    labels_train = labels[: n_train + n_valid]
+    features_test = features[(n_valid + n_train): ]
+    labels_test = labels[(n_valid + n_train):]
+    print("n_test", len(labels_test))
+    clf = svm.SVC()
+    clf.fit(features_train, labels_train)
+    y_pred = clf.predict(features_test)
+
+    prec, rec, f1, _ = precision_recall_fscore_support(labels_test, y_pred, average="binary")
+    print(prec, rec, f1)
+
+
 def gen_aff_record_linkage_table():
     pairs, labels = load_aff_data()
     pairs, labels = sklearn.utils.shuffle(
@@ -228,4 +406,7 @@ if __name__ == "__main__":
     # fit_tfidf_for_aff()
     # aff_keyword_method()
     # aff_svm()
-    gen_aff_record_linkage_table()
+    # gen_aff_record_linkage_table()
+    # fit_tfidf_for_venue()
+    # venue_keyword_method()
+    venue_svm()
